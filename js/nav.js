83ec45b563512c9e4141b1e17eb5d9f6013a0d5b
Fluxym-BBA <bbartoli@fluxym.com>
2026-08-21 16:19:07 +0200

    Add files via upload
    feat(plateformes-agreees): outillage de l'analyse 360 + acces au referentiel
    
    - nav.js : le referentiel des plateformes agreees n'etait accessible depuis
      AUCUN lien du menu ni du footer. Ajout dans Ecosysteme et dans Ressources,
      avec la nouvelle page methodologie
    - pa-taxonomie.json : facettes centralitePA (5 niveaux indices 0-4),
      secteurReferences, typeActionnaire ; 11 entrees de fiche pour les blocs
      analyse360 et identiteInternationale ; section blocsStructures decrivant
      le schema et les 14 chemins a fusion profonde ; interdits structurants
      inscrits dans _meta ; doublon groupeCapitalistique supprime
    - merge-plateformes.html : fusion profonde generique et recursive en
      remplacement du cas particulier socleTechnique, chemins lus dans la
      taxonomie avec repli interne, 10 compteurs de couverture 360
    - methodologie-plateformes.html : page publique. Declaration d'interet
      Fluxym, echelle de centralite, sources, disponibilite reelle des donnees,
      9 regles de publication, droit de reponse

--- js/nav.js
+++ js/nav.js
@@ -66,7 +66,6 @@ const SiteNav = {
                     <ul class="nav-submenu">
                         <li><a href="./champs.html">🔧 Les champs</a></li>
                         <li><a href="./cas-usage.html">📋 Cas d'usage</a></li>
-                        <li><a href="./validation.html">🔍 Validation</a></li>
                         <li><a href="./technique.html">⚙️ APIs & technique</a></li>
                         <li><a href="./regles.html">📏 Les règles</a></li>
                         <li><a href="./se-preparer.html">🚀 Se préparer</a></li>
@@ -79,16 +78,15 @@ const SiteNav = {
                         <li><a href="./faq.html">❓ FAQ</a></li>
                         <li><a href="./ressources.html">📚 Liens & outils</a></li>
                         <li><a href="./referentiel.html">🏷️ Référentiel & versions</a></li>
+                        <li><a href="./methodologie-plateformes.html">🔍 Méthodologie du référentiel PA</a></li>
                     </ul>
                 </li>
                 <li class="nav-dropdown">
                     <a href="./outils.html" class="nav-link nav-cta">🧰 Outils <span class="nav-arrow">▾</span></a>
                     <ul class="nav-submenu">
                         <li><a href="./outils.html">🧰 Tous les outils</a></li>
-                        <li><a href="./generateur.html">🏭 La Fabrique de factures</a></li>
-                        <li><a href="./validateur.html">🔍 Validateur XSD & Schematron</a></li>
+                        <li><a href="./generateur.html">🧪 Générateur UBL</a></li>
                         <li><a href="./transcodification.html">🧭 Matrice de transcodification</a></li>
-                        <li><a href="./tutoriels.html">🎬 Guide vidéo</a></li>
                     </ul>
                 </li>
             </ul>
@@ -125,8 +123,6 @@ const SiteNav = {
                             <li><a href="./e-invoicing.html">E-invoicing</a></li>
                             <li><a href="./e-reporting.html">E-reporting</a></li>
                             <li><a href="./chorus-pro.html">Chorus Pro</a></li>
-                            <li><a href="./b2g.html">B2G / G2B</a></li>
-                            <li><a href="./tva-preremplie.html">TVA pré-remplie</a></li>
                             <li><a href="./peppol.html">Peppol</a></li>
                             <li><a href="./sanctions.html">Sanctions</a></li>
                         </ul>
@@ -144,9 +140,7 @@ const SiteNav = {
                     <div class="footer-col">
                         <h4>Mettre en pratique</h4>
                         <ul>
-                            <li><a href="./champs.html">Les champs</a></li>
                             <li><a href="./cas-usage.html">Cas d'usage</a></li>
-                            <li><a href="./validation.html">Validation</a></li>
                             <li><a href="./technique.html">APIs & technique</a></li>
                             <li><a href="./regles.html">Les règles</a></li>
                             <li><a href="./se-preparer.html">Se préparer</a></li>
@@ -158,12 +152,11 @@ const SiteNav = {
                             <li><a href="./glossaire.html">Glossaire A→Z</a></li>
                             <li><a href="./faq.html">FAQ</a></li>
                             <li><a href="./ressources.html">Liens & outils</a></li>
-                            <li><a href="./referentiel.html">Référentiel &amp; versions</a></li>
-                            <li><a href="./outils.html">Les outils</a></li>
-                            <li><a href="./generateur.html">La Fabrique de factures</a></li>
-                            <li><a href="./validateur.html">Validateur XSD &amp; Schematron</a></li>
-                            <li><a href="./transcodification.html">Matrice de transcodification</a></li>
-                            <li><a href="./tutoriels.html">Guide vidéo</a></li>
+                            <li><a href="./referentiel.html">🏷️ Référentiel & versions</a></li>
+                            <li><a href="./methodologie-plateformes.html">🔍 Méthodologie du référentiel PA</a></li>
+                            <li><a href="./outils.html">🧰 Les outils</a></li>
+                            <li><a href="./generateur.html">🧪 Générateur UBL</a></li>
+                            <li><a href="./transcodification.html">🧭 Matrice de transcodification</a></li>
                         </ul>
                     </div>
                     <div class="footer-col">
